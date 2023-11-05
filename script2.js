cfe2b002e69be63d48fdc1c268aaa7cf0e329523
SupriyaSamsani <supriyasamsani@gmail.com>
2023-11-05 22:29:46 +0530

    initial

---
+++ script2.js
@@ -0,0 +1,45 @@
+const activeCellElement = document.querySelector(".active-cell");
+const fontsizeInput = document.querySelector("#fontsize");
+const fontfamilyInput = document.querySelector("#fontfamily");
+
+let activeElement = null;
+const state = {};
+const defaultProperties = {
+    fontfamily: 'sans',
+    fontSize: 16,
+    textAllign: "left",
+    color: "#000",
+    backgroundcolor: "#fff",
+    isBold: false,
+    isItalic: false,
+    isUnderline: false,
+}
+function onCellFocus(event){
+    const elementId = event.target.id;
+    activeCellElement.innerText = elementId;
+    //console.log(event.target.id)
+    activeElement = event.target;
+    if(state[elementId]){
+        resetOptions(state[elementId]);
+    }
+    else{
+        resetOptions(defaultProperties); 
+    }
+}
+function resetOptions(optionsState){
+
+    fontsizeInput.value = optionsState.fontSize;
+    fontfamilyInput.value = optionsState.fontfamily;
+}
+resetOptions(
+{
+fontfamily: 'monospace',
+fontSize: 16,
+textAllign: "left",
+color: "#000",
+backgroundcolor: "#fff",
+isBold: false,
+isItalic: false,
+isUnderline: false,
+}
+);
